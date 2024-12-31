e17c285e2456a8e692ab031f3293bc5a827c435b
Vishal <vishalkumarsinghv60@gmail.com>
2024-12-31 22:38:27 +0530

    New Commit

--- Polymorphism/src/ctp/Check.java
+++ Polymorphism/src/ctp/Check.java
@@ -1,9 +1,9 @@
 package ctp;
 
-public class Check
+public class Check 
 {
 	public static void main(String[] args) {
-		System.out.println("This is just to check main function working or not");
+		System.out.println("Polymorphism check");
 	}
 
 }
